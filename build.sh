3d1a80dc131a129645c1c63b2fc23d0b1b06cf04
ShahzaibH414 <shahzaibhassan414@gmail.com>
2026-01-05 19:22:20 +0500

    added vercel build 2

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
+# Set a Flutter version
+FLUTTER_VERSION="3.19.6"
+
+# Clone the Flutter repository with the specified version
+git clone https://github.com/flutter/flutter.git --depth 1 --branch $FLUTTER_VERSION
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Run flutter doctor to verify the installation
+flutter doctor
+
+# Build the Flutter web application
 flutter build web --release
