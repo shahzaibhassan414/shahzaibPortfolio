54118d4e2445ee5bea4d703b72ff4a7b4c6fb24c
ShahzaibH414 <shahzaibhassan414@gmail.com>
2026-01-06 15:44:42 +0500

    removed build.sh and vercel.json

--- lib/view/splash/splash.dart
+++ lib/view/splash/splash.dart
@@ -58,7 +58,6 @@ class _SplashScreenState extends State<SplashScreen>
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Portfolio v2'),
 
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.35,
