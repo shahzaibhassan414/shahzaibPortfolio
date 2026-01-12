11696370ea3b4e7686bcff85fa012f099b9fca02
ShahzaibH414 <shahzaibhassan414@gmail.com>
2026-01-12 15:51:58 +0500

    added keywords

--- lib/view/widget/rightPane.dart
+++ lib/view/widget/rightPane.dart
@@ -5,69 +5,68 @@ import 'package:portfolio/resource/appClass.dart';
 import '../../controller/generalController.dart';
 import '../../resource/colors.dart';
 
-class RightPane extends StatefulWidget {
+class RightPane extends ConsumerStatefulWidget {
   const RightPane({Key? key}) : super(key: key);
 
   @override
-  State<RightPane> createState() => _RightPaneState();
+  ConsumerState<RightPane> createState() => _RightPaneState();
 }
 
-class _RightPaneState extends State<RightPane> {
+class _RightPaneState extends ConsumerState<RightPane> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
         width: 60,
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Expanded(
-                  flex: 5,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Consumer(builder: (context, ref, child) {
-                        String val = ref.watch(hoverProvider);
-                        return Padding(
-                          padding: const EdgeInsets.only(bottom: 25),
-                          child: RotatedBox(
-                              quarterTurns: 1,
-                              child: InkWell(
-                                onTap: () async {
-                                  AppClass().openEmail(AppClass.email);
-                                },
-                                onHover: (bol) {
-                                  if (bol) {
-                                    ref.read(hoverProvider.notifier).state =
-                                        "email";
-                                  } else {
-                                    ref.read(hoverProvider.notifier).state = "";
-                                  }
-                                },
-                                child: Container(
-                                  child: Text(
-                                    AppClass.email,
-                                    style: TextStyle(
-                                        letterSpacing: 1,
-                                        color: val == "email"
-                                            ? AppColors().primaryRedColor
-                                            : AppColors().textColor,
-                                        fontSize: 14,
-                                        fontFamily: 'sfmono'),
-                                  ),
-                                ),
-                              )),
-                        );
-                      }),
-                    ],
-                  )),
-              Expanded(
-                  child: Container(
-                width: 1,
-                color: Colors.white,
-              ))
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+                flex: 5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Consumer(builder: (context, ref, child) {
+                      String val = ref.watch(hoverProvider);
+                      bool isHovered = val == "email";
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 25),
+                        child: RotatedBox(
+                          quarterTurns: 1,
+                          child: InkWell(
+                            onTap: () async {
+                              AppClass().openEmail(AppClass.email);
+                            },
+                            onHover: (bol) {
+                              ref.read(hoverProvider.notifier).state = bol ? "email" : "";
+                            },
+                            child: AnimatedContainer(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeInOut,
+                              margin: EdgeInsets.only(bottom: isHovered ? 8.0 : 0),
+                              child: Text(
+                                AppClass.email,
+                                style: TextStyle(
+                                    letterSpacing: 1.5,
+                                    color: isHovered
+                                        ? AppColors().primaryRedColor
+                                        : AppColors().textColor,
+                                    fontSize: 14,
+                                    fontWeight: isHovered ? FontWeight.bold : FontWeight.normal,
+                                    fontFamily: 'sfmono'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                  ],
+                )),
+            Expanded(
+                child: Container(
+              width: 1,
+              color: AppColors().textColor.withOpacity(0.5),
+            ))
+          ],
         ));
   }
 }
